99a7784ae05decd4f196d1fa5ee7ed13f370b38d
TheN8Learner <dalphaoumar27000@gmail.com>
2026-05-25 11:12:24 +0000

    feat: add commands for adding clients, checking status, and listing overdue clients in Telegram bot

--- telegram_bot.py
+++ telegram_bot.py
@@ -149,6 +149,89 @@ async def invoice(update: Update, context: ContextTypes.DEFAULT_TYPE):
                 text=f"{' '.join(context.args)} not found"
             )
 
+async def add(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    gc = gspread.service_account("sheetscripts.json")
+    sh = gc.open("Script")
+    worksheet = sh.get_worksheet(0)
+    data = worksheet.get_all_records()
+    dernier_index = len(data) 
+    row = dernier_index + 2
+    client = context.args
+    if not client:
+        await context.bot.send_message(
+        chat_id=update.effective_chat.id,
+        text=f"i will need the client name"
+    )
+    else:
+        infos = client[0].split('+')
+        if infos:
+            full_client = infos[0].split('_')
+            if full_client:
+                worksheet.update_cell(row, 1, ' '.join(full_client))
+            else:
+                worksheet.update_cell(row, 1, full_client)
+            worksheet.update_cell(row, 2, infos[1])
+            worksheet.update_cell(row, 3, infos[2])
+            worksheet.update_cell(row, 4, infos[3])
+            worksheet.update_cell(row, 5, infos[4])
+            worksheet.update_cell(row, 6, infos[5])
+            await context.bot.send_message(
+                chat_id=update.effective_chat.id,
+                text=f"{' '.join(full_client)} got added successfully"
+            )
+        else:
+            await context.bot.send_message(
+                chat_id=update.effective_chat.id,
+                text="i will need full infos of client"
+            )
+
+async def status(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    gc = gspread.service_account(filename="sheetscripts.json")
+    sh = gc.open(title="Script")
+    worksheet = sh.get_worksheet(0)
+    data = worksheet.get_all_records()
+    found = False
+    for client in data:
+        if client["Client name"].lower() == ' '.join(context.args).lower():
+            found = True
+            await context.bot.send_message(
+            chat_id=update.effective_chat.id,
+            text=f'{' '.join(context.args)} status is {client['Status']}'
+            )
+            break
+    if not found:
+        await context.bot.send_message(
+            chat_id=update.effective_chat.id,
+            text=f'{' '.join(context.args)} not found'
+        )
+
+async def overdue(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    gc = gspread.service_account(filename="sheetscripts.json")
+    sh = gc.open(title="Script")
+    worksheet = sh.get_worksheet(0)
+    data = worksheet.get_all_records()
+    today = datetime.today().date()
+    clients = []
+
+    for client in data:
+        due_date = datetime.strptime(client['Due Date'], '%Y-%m-%d').date()
+        is_overdue = today > due_date
+        status = client['Status'] == 'Paid'
+
+        if is_overdue and not status:
+            clients.append(client['Client name'])
+    
+    if clients == []:
+        await context.bot.send_message(
+            chat_id=update.effective_chat.id,
+            text='No overdued clients found'
+        )
+    else:
+        await context.bot.send_message(
+            chat_id=update.effective_chat.id,
+            text=f'Clients : {', '.join(clients)}'
+        )
+
 if __name__ == '__main__':
     # Initialize the application with your bot's token
     application = ApplicationBuilder().token(token).build()
@@ -160,6 +243,9 @@ if __name__ == '__main__':
     reminded_handler = CommandHandler('reminded', reminded)
     paid_handler = CommandHandler('paid', paid)
     invoice_handler  = CommandHandler('invoice', invoice)
+    add_handler = CommandHandler('add', add)
+    status_handler = CommandHandler('status', status)
+    overdue_handler = CommandHandler('overdue', overdue)
 
     
     application.add_handler(start_handler)
@@ -168,6 +254,9 @@ if __name__ == '__main__':
     application.add_handler(reminded_handler)
     application.add_handler(paid_handler)
     application.add_handler(invoice_handler)
+    application.add_handler(add_handler)
+    application.add_handler(status_handler)
+    application.add_handler(overdue_handler)
 
 
     # Run the bot until you press Ctrl-C
